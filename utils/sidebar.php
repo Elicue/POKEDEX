887c100d4a11cf99d589bfb150570285a7a8acf7
Lucie Ehrsam <lucie.ehrsam@edu.devinci.fr>
2023-01-14 21:02:26 +0100

    :lipstick: Add Sidebar

---
+++ utils/sidebar.php
@@ -0,0 +1,22 @@
+
+<div class="sideBar flex flex-col py-3 bg-white text-grisFonce justify-between w-[12%] shadow-md fixed h-screen text-lg">
+  <button class="All bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-black uppercase text-black">All</button>
+  <button class="Normal bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-normal uppercase text-normal">Normal</button>
+  <button class="Fighting bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-fighting uppercase text-fighting">Fighting</button>
+  <button class="Flying bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-flying uppercase text-flying">Flying</button>
+  <button class="Poison bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-poison uppercase text-poison">Poison</button>
+  <button class="Ground bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-ground uppercase text-ground">Ground</button>
+  <button class="Rock bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-rock uppercase text-rock">Rock</button>
+  <button class="Bug bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-bug uppercase text-bug">Bug</button>
+  <button class="Ghost bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-ghost uppercase text-ghost">Ghost</button>
+  <button class="Steel bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-steel uppercase text-steel">Steel</button>
+  <button class="Fire bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-fire uppercase text-fire">Fire</button>
+  <button class="Water bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-water uppercase text-water">Water</button>
+  <button class="Grass bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-grass uppercase text-grass">Grass</button>
+  <button class="Electric bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-electric uppercase text-electric">Electric</button>
+  <button class="Psychic bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-psychic uppercase text-psychic">Psychic</button>
+  <button class="Ice bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-ice uppercase text-ice">Ice</button>
+  <button class="Dragon bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-dragon uppercase text-dragon">Dragon</button>
+  <button class="Fairy bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-fairy uppercase text-fairy">Fairy</button>
+  <button class="Dark bg-gris p-1 rounded-md w-10/12 mx-auto focus:text-gris focus:bg-dark uppercase text-dark">Dark</button>
+</div>
